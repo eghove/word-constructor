664a2116cb2d87b4d70dc1c67063f75a7cba65fd
That_Guy <eghove@gmail.com>
2018-12-04 16:51:13 -0600

    first version of the Letter constructor

---
+++ Letter.js
@@ -0,0 +1,31 @@
+//for the Letter constructor
+
+let Letter = function (letter, guess) {
+    this.actualLetter = letter.toString();
+    this.beenGuessed = false;
+    this.attempt = guess;
+    this.display = function () {
+        if (this.beenGuessed===true) {
+            return this.actualLetter;
+        } else {
+            return "__";
+        }
+    }
+
+    this.guessing = function () {
+        if (this.attempt==this.actualLetter) {
+            this.beenGuessed = true;
+        } else {
+            this.beenGuessed = false;
+        }
+        return this.display();
+    }
+}
+
+// let guess1 = new Letter ('H', 'L');
+// var test=guess1.guessing();
+// console.log(guess1.actualLetter + " " + guess1.attempt);
+// console.log(test);
+
+//exporting this so other files can use it.
+module.exports = Letter;
